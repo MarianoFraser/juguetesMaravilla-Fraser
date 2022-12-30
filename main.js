7f9c181e64d3b725a98241749507eb5e5b31f751
Mariano Fraser <mariano.fraser@gmail.com>
2022-12-29 21:44:44 -0300

    se agrego dom

--- main.js
+++ main.js
@@ -1,4 +1,146 @@
-//* Creacion de objetos (Descuentos)
+//* Creacion de objeto productos 
+
+class Producto {
+    constructor(id, nombre, precio, img){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+        this.cantidad = 1;
+    }
+}
+
+const pasadorCordones = new Producto(1, "pasadorCordones", 1000,"img/pasadorCordones.jpeg");
+const rompecabezas = new Producto(2, "rompecabezas", 1500,"img/rompecabezas.jpeg");
+const secuencias = new Producto(3, "secuencias", 3000,"img/secuencias.jpeg");
+const ruleta = new Producto(4, "ruleta", 4000,"img/ruleta.jpeg");
+const motricidad = new Producto(5, "motricidad", 5000,"img/motricidad.jpeg");
+
+const productos = [pasadorCordones, rompecabezas, secuencias, ruleta, motricidad];
+
+//* creacion del array del carrito
+
+let carrito = [];
+
+//* local storage
+
+if(localStorage.getItem("carrito")){
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+}
+
+//* creacion objeto de usuarios
+
+
+
+//* modificamos DOM
+const contenedorProductos = document.getElementById("contenedorProductos");
+
+//* Funcion para mostrar Productos y agregar al carrito
+
+const mostrarProductos = () => {
+    productos.forEach( producto => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.innerHTML = `
+            <div class="card">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+                <div class= "card-body">
+                    <h5> ${producto.nombre}</h5>
+                    <p> $${producto.precio}</p>
+                    <button class="btn colorBoton" id="boton${producto.id}" > Agregar al Carrito </button>
+                </div>
+            </div>
+            `
+        contenedorProductos.appendChild(card);
+        
+        //* Agregar productos al carrito
+        const boton = document.getElementById(`boton${producto.id}`);
+        boton.addEventListener("click",() => {
+            agregarAlCarrito(producto.id);
+        })
+    })
+}
+mostrarProductos();
+
+const agregarAlCarrito = (id) => {
+    const productoEnCarrito = carrito.find(producto => producto.id === id);
+    if(productoEnCarrito){
+        productoEnCarrito.cantidad++;
+    }else {
+        const producto = productos.find(producto => producto.id === id);
+        carrito.push(producto);
+    }
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    calcularTotal();
+}
+
+//* Mostrar y modificar el carrito de compras
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const verCarrito = document.getElementById("verCarrito");
+
+verCarrito.addEventListener("click", () => {
+    mostrarCarrito();
+})
+
+
+const mostrarCarrito = () => {
+    contenedorCarrito.innerHTML = "";
+    carrito.forEach(producto => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.innerHTML = `
+            <div class="card">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+                <div class= "card-body">
+                    <h5> ${producto.nombre}</h5>
+                    <p> $${producto.precio}</p>
+                    <p> cantidad: ${producto.cantidad}</p>
+                    <button class="btn colorBoton" id="eliminar${producto.id}" > Eliminar producto </button>
+                </div>
+            </div>
+            `
+        contenedorCarrito.appendChild(card);
+        const boton = document.getElementById(`eliminar${producto.id}`);
+        boton.addEventListener("click", () =>{
+            eliminarDelCarrito(producto.id);
+        })
+    })
+    calcularTotal();
+}
+//Funcion eliminar producto del carrito
+
+const eliminarDelCarrito = (id) => {
+    const producto = carrito.find(producto => producto.id === id);
+    const indice = carrito.indexOf(producto);
+    carrito.splice(indice, 1);
+    mostrarCarrito();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+//Funcion vaciar carrito
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+vaciarCarrito.addEventListener("click", () => {
+    eliminarTodoCarrito();
+})
+
+const eliminarTodoCarrito = () => {
+    carrito = [];
+    mostrarCarrito();
+    localStorage.clear();
+}
+
+//Mostramos mensaje con el total de la compra
+
+const total = document.getElementById("total");
+const calcularTotal = () => {
+    let totalCompra = 0;
+    carrito.forEach(producto => {
+        totalCompra += producto.precio * producto.cantidad;
+    })
+    total.innerHTML = `: $${totalCompra}`;
+}
 
 class Usuario{
     constructor(usuario, contraseña, descuento){
@@ -13,73 +155,8 @@ const camifra = new Usuario("camifra", "5678", 0.75);
 
 const arrayUsuarios = [marianofra, camifra];
 
-//* reconocimiento de usuario y calculo de descuento
+//* Funcion loguearse
 
-let descuentoCompra = 1;
-let tipoDeUsuario = parseInt (prompt ("Bienvenido, por favor identifiquese para continuar: \n\n1)Crear usuario \n2)Loguear con usuario existente \n3)Ingresar como visitante"));
-switch(tipoDeUsuario){
-    case 1:
-        alert("proximamente, ojala vuelvas pronto, te dejamos 10% de descuento")
-        descuentoCompra = 0.9;
-        break;
-    case 2:
-        let usuarioLogeo = prompt ("Ingrese Usuario");
-        let contraseñaLogeo = prompt ("Ingrese Contraseña");
-        const buscado = arrayUsuarios.find(usuarios => usuarios.usuario === usuarioLogeo);
-        if(buscado != undefined){
-            if(contraseñaLogeo === buscado.contraseña){
-                descuentoCompra = buscado.descuento;
-            }else{
-                alert("Usuario invalido - se ingresa como visitante");
-            }
-        }else{
-            alert("Usuario invalido - se ingresa como visitante");
-        }
-        break;
-    case 3:
-        descuentoCompra = 1;
-        break;
-}
+const contenedorUsuario = document.getElementById("contenedorUsuario");
 
-//* MARKET SUMATORIA DE PRODUCTOS Y CALCULO IVA
-
-let seleccionarProductos = parseInt (prompt ("Seleccione el producto deseado: \n\n1)Pasador de cordones $1000 + iva \n2)Rompecabezas $1500 + iva \n3)Secuencias $3000 + iva \n4)Ruleta AVD  $4000 + iva \n0)Volver al inicio"));
-
-//* ciclo celeccionar nuevamente producto hasta ser correcto, a los 5 ingresos no validos vuelve
-let numeroErrores = 0
-while (seleccionarProductos <0 || seleccionarProductos >4){
-    seleccionarProductos = parseInt (prompt ("Seleccione nuevamente un producto del 1 al 4"));
-    numeroErrores = numeroErrores + 1;
-    if (numeroErrores >= 5){
-        seleccionarProductos = 0;
-    }
-}
-
-//* ingreso cantidad de productos deseados si se eligio bien el producto
-let seleccionarCantidad = 0;
-if(seleccionarProductos >0 && seleccionarProductos <5){
-    seleccionarCantidad = parseInt (prompt ("Seleccione la cantidad deseada"));
-}
-
-//*Funcion flecha para calculo IVa y descuento
-let productoMasIva = (a,b) => {
-    return a * b * 1.21 * descuentoCompra;
-}
-
-switch(seleccionarProductos){
-    case 1:
-        alert("El costo final con iva es $" + productoMasIva(1000,seleccionarCantidad));
-        break;
-    case 2:
-        alert("El costo final con iva es $" + productoMasIva(1500,seleccionarCantidad));
-        break;
-    case 3:
-        alert("El costo final con iva es $" + productoMasIva(3000,seleccionarCantidad));
-        break;
-    case 4:
-        alert("El costo final con iva es $" + productoMasIva(4000,seleccionarCantidad));
-        break;
-    default:
-        alert("Se selecciono Volver");
-}
 
